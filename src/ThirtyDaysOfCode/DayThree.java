3e7a2cdbc4b422bddece58e3d7c33a1fb216226b
Adrian Brown <adrian.r.brown90@gmail.com>
2022-06-04 20:35:58 -0700

    adding necessary imports to get rid of warnings

--- src/ThirtyDaysOfCode/DayThree.java
+++ src/ThirtyDaysOfCode/DayThree.java
@@ -1,5 +1,9 @@
 package ThirtyDaysOfCode;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class DayThree {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
--- src/ThirtyDaysOfCode/DayTwo.java
+++ src/ThirtyDaysOfCode/DayTwo.java
@@ -1,5 +1,9 @@
 package ThirtyDaysOfCode;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class DayTwo {
     /*
      * Complete the 'solve' function below.
