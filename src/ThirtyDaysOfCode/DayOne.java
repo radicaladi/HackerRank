bd92190eca1c93d42f231402e5fd7cf7cb448e56
Adrian Brown <adrian.r.brown90@gmail.com>
2022-06-04 20:14:00 -0700

    refactoring names for correct days

--- src/ThirtyDaysOfCode/DayOne.java
+++ src/ThirtyDaysOfCode/DayOne.java
@@ -4,20 +4,33 @@ import java.util.Scanner;
 
 public class DayOne {
     public static void main(String[] args) {
-        // Create a Scanner object to read input from stdin.
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
         Scanner scan = new Scanner(System.in);
 
-        // Read a full line of input from stdin and save it to our variable, inputString.
-        String inputString = scan.nextLine();
+        /* Declare second integer, double, and String variables. */
+        int j = scan.nextInt();
+        double k = scan.nextDouble();
+        String l = "";
+        scan.nextLine();
+        l = scan.nextLine();
 
-        // Close the scanner object, because we've finished reading
-        // all inputs from stdin needed for this challenge.
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back
+        // and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i + j);
+
+        /* Print the sum of the double variables on a new line. */
+        System.out.println(d + k);
+
+        /* Concatenate and print the String variables on a new line;
+        	the 's' variable above should be printed first. */
+        System.out.println(s + l);
+
         scan.close();
-
-        // Print a string literal saying "Hello, World." to stdout.
-        System.out.println("Hello, World.");
-
-        // TODO: Write a line of code here that prints the contents of inputString to stdout.
-        System.out.print(inputString);
     }
 }
