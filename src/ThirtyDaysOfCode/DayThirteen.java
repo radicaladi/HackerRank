ef5150a91daaa58454396f644de8a1688e37fab0
Adrian Brown <adrian.r.brown90@gmail.com>
2022-06-14 21:24:26 -0700

    commenting out code for IDE warning resolution

--- src/ThirtyDaysOfCode/DayThirteen.java
+++ src/ThirtyDaysOfCode/DayThirteen.java
@@ -1,53 +1,53 @@
 package ThirtyDaysOfCode;
 
 public class DayThirteen {
-    //TODO: Declare your class here. Do not use the 'public' access modifier.
-    class MyBook extends Book {
-        //TODO: Declare the price instance variable
-        int price;
+//    //TODO: Declare your class here. Do not use the 'public' access modifier.
+//    class MyBook extends Book {
+//        //TODO: Declare the price instance variable
+//        int price;
+//
+//        /**
+//         *   Class Constructor
+//         *
+//         *   @param title The book's title.
+//         *   @param author The book's author.
+//         *   @param price The book's price.
+//         **/
+//        //TODO: Write your constructor here
+//        MyBook (String title, String author, int price) {
+//            super(title, author);
+//            this.price = price;
+//        }
+//
+//        /**
+//         *   Method Name: display
+//         *
+//         *   Print the title, author, and price in the specified format.
+//         **/
+//        //TODO: Write your method here
+//        public void display() {
+//            System.out.println(
+//                    "Title: " + this.title + "\n" +
+//                            "Author: " + this.author + "\n" +
+//                            "Price: " + this.price + "\n"
+//            );
+//        }
+//
+//
+//// End class
+//    }
 
-        /**
-         *   Class Constructor
-         *
-         *   @param title The book's title.
-         *   @param author The book's author.
-         *   @param price The book's price.
-         **/
-        //TODO: Write your constructor here
-        MyBook (String title, String author, int price) {
-            super(title, author);
-            this.price = price;
-        }
-
-        /**
-         *   Method Name: display
-         *
-         *   Print the title, author, and price in the specified format.
-         **/
-        //TODO: Write your method here
-        public void display() {
-            System.out.println(
-                    "Title: " + this.title + "\n" +
-                            "Author: " + this.author + "\n" +
-                            "Price: " + this.price + "\n"
-            );
-        }
-
-
-// End class
-    }
-
-    public class Solution {
-
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            String title = scanner.nextLine();
-            String author = scanner.nextLine();
-            int price = scanner.nextInt();
-            scanner.close();
-
-            Book book = new MyBook(title, author, price);
-            book.display();
-        }
-    }
+//    public class Solution {
+//
+//        public static void main(String[] args) {
+//            Scanner scanner = new Scanner(System.in);
+//            String title = scanner.nextLine();
+//            String author = scanner.nextLine();
+//            int price = scanner.nextInt();
+//            scanner.close();
+//
+//            Book book = new MyBook(title, author, price);
+//            book.display();
+//        }
+//    }
 }
