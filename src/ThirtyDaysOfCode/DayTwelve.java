7a107bd2356e7a143c6d004653360ab020139e7f
Adrian Brown <adrian.r.brown90@gmail.com>
2022-06-14 21:23:13 -0700

    correecting comments

--- src/ThirtyDaysOfCode/DayTwelve.java
+++ src/ThirtyDaysOfCode/DayTwelve.java
@@ -1,11 +1,11 @@
-//package ThirtyDaysOfCode;
-//
-//// You are given two classes, Person and Student, where Person is the base class and Student is the derived class.
-//// Completed code for Person and a declaration for Student are provided for you in the editor.
-//// Observe that Student inherits all the properties of Person.
-//
-//public class DayTwelve {
-//    // for purpose of challenge record, I am keeping this class as the day of challenge rather than converting to "Student" extends "Person"
+package ThirtyDaysOfCode;
+
+// You are given two classes, Person and Student, where Person is the base class and Student is the derived class.
+// Completed code for Person and a declaration for Student are provided for you in the editor.
+// Observe that Student inherits all the properties of Person.
+
+public class DayTwelve {
+    // for purpose of challenge record, I am keeping this class as the day of challenge rather than converting to "Student" extends "Person"
 //    private int[] testScores;
 //
 //    /*
@@ -51,4 +51,4 @@
 //        } else
 //            return Character.MIN_VALUE;
 //    }
-//}
+}
