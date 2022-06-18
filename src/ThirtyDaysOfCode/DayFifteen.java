63619233d96a357bb5a0845fa313418e546ead6a
Adrian Brown <adrian.r.brown90@gmail.com>
2022-06-17 17:33:50 -0700

    commenting out code for IDE warning resolution

--- src/ThirtyDaysOfCode/DayFifteen.java
+++ src/ThirtyDaysOfCode/DayFifteen.java
@@ -5,15 +5,15 @@ import org.w3c.dom.Node;
 
 public class DayFifteen {
     // missing a few java utils like Scanner input
-    public static Node insert(Node head, int data) {
-        //Complete this method
-        if (head == null)
-            return new Node(data);
-        else if (head.next == null) {
-            head.next = new Node(data);
-        } else {
-            insert(head.next, data);
-        }
-        return head;
-    }
+//    public static Node insert(Node head, int data) {
+//        //Complete this method
+//        if (head == null)
+//            return new Node(data);
+//        else if (head.next == null) {
+//            head.next = new Node(data);
+//        } else {
+//            insert(head.next, data);
+//        }
+//        return head;
+//    }
 }
