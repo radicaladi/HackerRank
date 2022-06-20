2d50a66278a9a1cb8bd56c05460f0a4dd422b094
Adrian Brown <adrian.r.brown90@gmail.com>
2022-06-19 23:06:25 -0700

    adding day 17 solution of hackerrank 30 day challenge

---
+++ src/ThirtyDaysOfCode/DaySeventeen.java
@@ -0,0 +1,23 @@
+package ThirtyDaysOfCode;
+
+//TODO: Write a Calculator class with a single method: int power(int,int).
+// The power method takes two integers, n and p, as parameters and returns the integer result of n^p.
+// If either n or p is negative, then the method must throw an exception with the message: n and p should be non-negative.
+
+public class DaySeventeen {
+    //Write your code here
+//    class Calculator {
+//        public int power(final int n, final int p) {
+//            if (n < 0 || p < 0) {
+//                throw new IllegalArgumentException("n and p should be non-negative");
+//            }
+//
+//            int power = 1;
+//            for (int i = 0; i < p; i++) {
+//                power *= n;
+//            }
+//
+//            return power;
+//        }
+//    }
+}
