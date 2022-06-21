66d1c0dab03f45aa57bfebfaf3c077a1ab18cade
Adrian Brown <adrian.r.brown90@gmail.com>
2022-06-20 21:42:42 -0700

    adding day 18 solution of hackerrank 30 day challenge

---
+++ src/ThirtyDaysOfCode/DayEighteen.java
@@ -0,0 +1,26 @@
+package ThirtyDaysOfCode;
+
+import java.util.*;
+
+public class DayEighteen {
+    // Write your code here.
+    Stack<Character> stack = new Stack<>();
+    Queue<Character> queue = new LinkedList<>();
+
+    void pushCharacter(char ch) {
+        stack.push(ch);
+    }
+
+    void enqueueCharacter(char ch) {
+        queue.add(ch);
+    }
+
+    char popCharacter() {
+        return stack.pop();
+    }
+
+    char dequeueCharacter() {
+        return queue.remove();
+    }
+
+}
