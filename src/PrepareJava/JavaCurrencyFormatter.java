6c25356a4092c138a488d9b3fa358f19b8fa536f
Adrian Brown <adrian.r.brown90@gmail.com>
2022-07-07 11:48:56 -0700

    adding hackerrank java currency formatter solution

--- src/PrepareJava/JavaCurrencyFormatter.java
+++ src/PrepareJava/JavaCurrencyFormatter.java
@@ -1,2 +1,26 @@
-package PrepareJava;public class JavaCurrencyFormatter {
+package PrepareJava;
+
+import java.util.*;
+import java.text.*;
+
+public class JavaCurrencyFormatter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        Locale in = new Locale("en", "IN");
+        String india = NumberFormat.getCurrencyInstance(in).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
 }
+
