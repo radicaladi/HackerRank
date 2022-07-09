47039b1c2bee48f1799b07b68ffc4bdaef6c1df4
Adrian Brown <adrian.r.brown90@gmail.com>
2022-07-08 20:42:47 -0700

    adding solution for day twenty of thirty days of code

--- src/ThirtyDaysOfCode/DayTwenty.java
+++ src/ThirtyDaysOfCode/DayTwenty.java
@@ -1,2 +1,46 @@
-package ThirtyDaysOfCode;public class DayTwenty {
+package ThirtyDaysOfCode;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DayTwenty {
+    public static int bubbleSortProgram(int[] a, int n) {
+        int numSwaps = 0;
+        for (int i = 0; i < n; i++) {
+            int numberOfSwaps = 0;
+
+            for (int j = 0; j < n - 1; j++) {
+                if (a[j] > a[j + 1]) {
+
+                    int temp = a[j + 1];
+                    a[j + 1] = a[j];
+                    a[j] = temp;
+
+                    numberOfSwaps++;
+                    numSwaps++;
+                }
+            }
+
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+        return numSwaps;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+        sc.close();
+
+        int numSwaps = bubbleSortProgram(a, n);
+
+        System.out.println("Array is sorted in " + numSwaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[n - 1]);
+    }
 }
