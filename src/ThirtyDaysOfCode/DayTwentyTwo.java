a84ed1f5570f4f05d2b473928b19bf9e6e3d3d65
Adrian Brown <adrian.r.brown90@gmail.com>
2022-07-10 13:18:01 -0700

    adding solution for day 22 of hackerrank 30 days of code java

--- src/ThirtyDaysOfCode/DayTwentyTwo.java
+++ src/ThirtyDaysOfCode/DayTwentyTwo.java
@@ -1,2 +1,22 @@
-package ThirtyDaysOfCode;public class DayTwentyTwo {
+package ThirtyDaysOfCode;
+
+import org.w3c.dom.Node;
+
+//The height of a binary search tree is the number of edges between the tree's root and its furthest leaf.
+// You are given a pointer, root, pointing to the root of a binary search tree.
+// Complete the getHeight function provided in your editor so that it returns the height of the binary search tree.
+
+public class DayTwentyTwo {
+
+    public static int getHeight(Node root){
+        //Write your code here
+        if (root == null)
+            return -1;
+        else
+        {
+            int leftDepth = getHeight(root.left);
+            int rightDepth = getHeight(root.right);
+            return Math.max(leftDepth+1, rightDepth+1);
+        }
+    }
 }
