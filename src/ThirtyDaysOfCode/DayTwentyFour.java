0e7ef7cc01fae16ff12a85472e669fbe5d50d5b5
Adrian Brown <adrian.r.brown90@gmail.com>
2022-07-12 12:21:27 -0700

    adding solution to day 24 of hackerrank 30 days of code java

--- src/ThirtyDaysOfCode/DayTwentyFour.java
+++ src/ThirtyDaysOfCode/DayTwentyFour.java
@@ -1,2 +1,30 @@
-package ThirtyDaysOfCode;public class DayTwentyFour {
+package ThirtyDaysOfCode;
+import org.w3c.dom.Node;
+
+/* TODO:
+* A Node class is provided for you in the editor.
+* A Node object has an integer data field, data, and a Node instance pointer, next ,
+* pointing to another node (i.e.: the next node in a list).
+A removeDuplicates function is declared in your editor,
+* which takes a pointer to the head node of a linked list as a parameter.
+* Complete removeDuplicates so that it deletes any duplicate nodes from the list and returns the head of the updated list.
+*/
+
+public class DayTwentyFour {
+    public static Node removeDuplicates(Node head) {
+        //Write your code here
+        if (head == null) {
+            return null;
+        }
+        Node temporary_node = head;
+        while(temporary_node.next != null) {
+            if(temporary_node.data != temporary_node.next.data) {
+                temporary_node = temporary_node.next;
+            }
+            else {
+                temporary_node.next = temporary_node.next.next;
+            }
+        }
+        return head;
+    }
 }
