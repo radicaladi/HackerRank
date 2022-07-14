849af61fc5a457f494a115387c226a4079f56e9a
Adrian Brown <adrian.r.brown90@gmail.com>
2022-07-13 20:03:19 -0700

    adding solution for day 25 of 30 days of code java

--- src/ThirtyDaysOfCode/DayTwentyFive.java
+++ src/ThirtyDaysOfCode/DayTwentyFive.java
@@ -1,4 +1,44 @@
 package ThirtyDaysOfCode;
 
+import java.util.Scanner;
+
+// TODO:
+// A prime is a natural number greater than that has no positive divisors other than and itself.
+// Given a number, n, determine and print whether it is prime or not prime .
+
 public class DayTwentyFive {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scan.nextInt();
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (isPrime(numbers[i]))
+                System.out.println("Prime");
+            else
+                System.out.println("Not prime");
+        }
+    }
+
+    public static boolean isPrime(int number) {
+
+        double numberSqrt = Math.sqrt(number);
+
+        if (number <= 1)
+            return false;
+
+        else {
+            for (int j = 2; j <= numberSqrt; j++) {
+                if (number % j == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
