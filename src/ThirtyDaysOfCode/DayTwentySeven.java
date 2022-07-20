0c6b068de5033aee9acc9aa4af98178674893d4c
Adrian Brown <adrian.r.brown90@gmail.com>
2022-07-19 18:51:52 -0700

    adding last test method

--- src/ThirtyDaysOfCode/DayTwentySeven.java
+++ src/ThirtyDaysOfCode/DayTwentySeven.java
@@ -1,5 +1,7 @@
 package ThirtyDaysOfCode;
 
+//This problem is about unit testing.
+
 public class DayTwentySeven {
     //TODO:
     //In the class TestDataEmptyArray:
@@ -17,6 +19,19 @@ public class DayTwentySeven {
     static class TestDataUniqueValues {
         public static int[] get_array() {
             return new int[] {0,1};
+        }
+        public static int get_expected_result() {
+            return 0;
+        }
+    }
+
+    //TODO:
+    //In the class TestDataExactlyTwoDifferentMinimums:
+    //get_array() returns an array where the minimum value occurs at exactly 2 indices
+    //get_expected_result() returns the expected index
+    static class TestDataExactlyTwoDifferentMinimums {
+        public static int[] get_array() {
+            return new int[] {5,5,8};
         }
         public static int get_expected_result() {
             return 0;
