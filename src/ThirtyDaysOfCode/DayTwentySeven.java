d90c0a3c64ecb51835974570782d6dd510c647e0
Adrian Brown <adrian.r.brown90@gmail.com>
2022-07-19 18:45:13 -0700

    adding testdatauniquevalues class and methods

--- src/ThirtyDaysOfCode/DayTwentySeven.java
+++ src/ThirtyDaysOfCode/DayTwentySeven.java
@@ -10,4 +10,16 @@ public class DayTwentySeven {
         }
     }
 
+    //TODO:
+    //in the class TestDataUniqueValues:
+    //get_array() returns an array of size at least 2 with all unique elements
+    //get_expected_result() returns the expected minimum value index for this array
+    static class TestDataUniqueValues {
+        public static int[] get_array() {
+            return new int[] {0,1};
+        }
+        public static int get_expected_result() {
+            return 0;
+        }
+    }
 }
